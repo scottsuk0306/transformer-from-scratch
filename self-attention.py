97043a055bd6492d31bc5d5cfdf55ab6ca3b50d0
scottsuk0306 <scottsuk0306@kaist.ac.kr>
2022-07-22 17:48:23 +0900

    Create self-attention.py

---
+++ self-attention.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+
+class SelfAttention(nn.Module):
+    def __init__(self, embed_size, heads):
+        super(SelfAttention, self).__init__()
+        self.embed_size = embed_size
+        self.heads = heads
+        # head_dim refers to dim of q, k, v
+        self.head_dim = embed_size // heads
+        
+        assert(self.head_dim * heads == embed_size, "Embed size needs to be div by heads") 
+        
+        self.values = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.fc_out = nn.Linear(heads*self.head_dim, embed_size)
+    
+    def forward(self, values, keys, query, mask):
+        N = query.shape[0]
+        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+        
+        # Split embedding into self.heads pieces
+        # How is this done?
+        values = values.reshape(N, value_len, self.heads, self.head_dim)
+        keys = keys.reshape(N, key_len, self.heads, self.head_dims)
+        queries = query.reshape(N, query_len, self.heads, self.head_dim)
+        
+        energy = torch.einsum("nqhd, nkhd->nhqk", [queries, keys])
+        # queries shape: (N, query_len, heads, head_dim)
+        # keys shape: (N, key_len, heads, head_dim)
+        # energy shape: (N, heads, query_len, key_len)
+        # query_len: Target sentence length
+        # key_len: Source sentence length
+        
+        if mask is not None:
+            energy = energy.masked_fill(mask == 0, float("-1e20"))
+        
+        attention = torch.softmax(energy / (self.embed_size ** (1/2)), dim = 3)
+        
+        out = torch.einsum("nhql,nlhd->nqhd", [attention, values]).reshape(
+            N, query_len, self.heads*self.head_dim
+        )
+        # attention shape: (N, heads, query_len, key_len)
+        # values shape: (N, value_len, heads, heads_dim)
+        # output shape: (N, query_len, heads, head_dim)
+        # Note that query_len and value_len are always the same, so we can multiply
+        # on that direction
+        # After einsum, then flatten last two dimensions.
+
+        out = self.fc_out(out)
+        return out
+        
+        
