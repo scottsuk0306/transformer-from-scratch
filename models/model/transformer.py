a45b57458a9f24a0f4734219ed90b994b7649b5f
scottsuk0306 <scottsuk0306@kaist.ac.kr>
2022-07-26 16:32:34 +0900

    Implement Transformer

--- models/model/transformer.py
+++ models/model/transformer.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 
 from models.layers.self_attention import SelfAttention
+from models.model import Decoder
+from models.model import Encoder
 
 def TransformerBlock(nn.Module):
     def __init__(self, embed_size, heads, dropout, forward_expansion):
@@ -25,4 +27,63 @@ def TransformerBlock(nn.Module):
         x = self.dropout(self.norm1(attention + query))
         forward = self.feed_forward(x)
         out = self.droput(self.norm2(forward + x))
+        return out
+
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        src_vocab_size,
+        trg_vocab_size,
+        src_pad_idx,
+        trg_pad_idx,
+        embed_size=256,
+        num_layers=6,
+        forward_expansion=4,
+        heads=8,
+        dropout=0,
+        device="cuda",
+        max_length=100
+    ):
+        super(Transformer, self).__init__()
+        
+        self.encoder = Encoder(
+            src_vocab_size,
+            embed_size,
+            num_layers,
+            heads,
+            device,
+            forward_expansion,
+            dropout,
+            max_length
+        )
+        
+        self.encoder = Decoder(
+            trg_vocab_size,
+            embed_size,
+            num_layers,
+            heads,
+            device,
+            forward_expansion,
+            dropout,
+            device,
+            max_length
+        )
+    
+    def make_src_mask(self, src):
+        src_mask = (src != self.src_pad_idx).unsqueeze(1).unsqueeze(2)
+        # (N, 1, 1, src_len)
+        return src_mask.to(self.device)
+
+    def make_trg_mask(self, trg):
+        N, trg_len = trg.shape
+        trg_mask = torch.tril(torch.ones(trg_len, trg_len)).expand(
+            N, 1, trg_len, trg_lens
+        )
+        return trg_mask.to(self.device)
+    
+    def forward(self, src, trg):
+        src_mask = self.make_src_mask(src)
+        trg_mask = self.make_trg_mask(trg)
+        enc_src = self.encoder(src, src_mask)
+        out = self.decoder(trg, enc_src, src_mask, trg_mask)
         return out
