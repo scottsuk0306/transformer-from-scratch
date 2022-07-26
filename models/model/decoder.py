577ce08d094d1786e0b14cb4ff667d030fa0122f
scottsuk0306 <scottsuk0306@kaist.ac.kr>
2022-07-27 02:39:54 +0900

    Separate DecoderBlock and Decoder

--- models/model/decoder.py
+++ models/model/decoder.py
@@ -1,24 +1,7 @@
 import torch
 import torch.nn as nn
 
-from models.layers.transformer_block import TransformerBlock
-from models.layers.self_attention import SelfAttention
-
-class DecoderBlock(nn.Module):
-    def __init__(self, embed_size, heads, forward_expansion, dropout, device):
-        super(DecoderBlock, self).__init__()
-        self.attention = SelfAttention(embed_size, heads)
-        self.norm = nn.LayerNorm(embed_size)
-        self.transformer_block = TransformerBlock(
-            embed_size, heads, dropout, forward_expansion
-        )
-        self.dropout = nn.Dropout(dropout)
-    
-    def forward(self, x, value, key, src_mask, trg_mask):
-        attention = self.attention(x, x, x, trg_mask)
-        query = self.dropout(self.norm(attention + x))
-        out = self.transformer_block(value, key, query, src_mask)
-        return out
+from models.layers.decoder_block import DecoderBlock
 
 class Decoder(nn.Module):
     def __init__(self, 
